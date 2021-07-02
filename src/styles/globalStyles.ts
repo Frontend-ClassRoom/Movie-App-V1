5c74a3eaebb2fb420b9ad78c76db8da8532be58f
전병길 <jeonbg@kakao.com>
2021-07-02 23:53:55 +0900

    style : globalStyle 임시수정

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -1,5 +1,10 @@
 import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
+
 export const GlobalStyle = createGlobalStyle`
   ${reset}
+  body {
+    width: 100vw;
+    height:100vh;
+  }
 `;
