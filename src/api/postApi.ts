82baad6d2d11f2ef25f6ea050bebbe3a538f1f58
ahnytae <ahnheetae@ucomp.co.kr>
2021-07-04 19:39:08 +0900

    고칠거 없지만 테스트커밋 하나

--- src/api/postApi.ts
+++ src/api/postApi.ts
@@ -2,14 +2,12 @@ import axios from "axios";
 import { Posts } from "types/posts";
 import { AxiosReturn } from "utils/AxiosReturn";
 
-const BASE_URL = "https://jsonplaceholder.typicode.com";
-
 export const getPosts = () => {
-  const url = `${BASE_URL}/posts`;
+  const url = `${process.env.REACT_APP_BASE_API}/posts`;
   return axios.get<Posts[]>(url).then((res) => res.data);
 };
 
 export const getPostDetail = (postId: number) => {
-  const url = `${BASE_URL}/posts/${postId}`;
+  const url = `${process.env.REACT_APP_BASE_API}/posts/${postId}`;
   return axios.get<Posts>(url).then(AxiosReturn);
 };
