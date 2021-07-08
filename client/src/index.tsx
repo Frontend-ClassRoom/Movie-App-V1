404c61ce6f456be04d2da0feeef37a50adb443cd
전병길 <jeonbg@kakao.com>
2021-07-09 00:21:41 +0900

    feat : redux-persis 추가 1. 새로고침시 store state 유지

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -6,15 +6,19 @@ import { Provider } from 'react-redux';
 import theme from 'assets/styles/theme';
 import { createStore } from 'redux';
 import rootReducer from 'store/reducer/rootReducer';
+import persistStore from 'redux-persist/es/persistStore';
+import { PersistGate } from 'redux-persist/integration/react';
 
-// const store = configureStore();
 const store = createStore(rootReducer);
+const persistor = persistStore(store);
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <PersistGate loading={null} persistor={persistor}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </PersistGate>
   </ThemeProvider>,
   document.getElementById('root')
 );
