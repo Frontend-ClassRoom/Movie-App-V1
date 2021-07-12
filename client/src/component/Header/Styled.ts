290cde91bd04b831b86b6ad3a4308759f1db6c72
전병길 <jeonbg@kakao.com>
2021-07-12 22:19:18 +0900

    feat : Header 컴포넌트 추가

---
+++ client/src/component/Header/Styled.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+import { transformEm } from 'assets/styles';
+
+export const StyledHeader = styled.div`
+  padding: ${transformEm(20)} ${transformEm(0)};
+`;
+
+export const StyledHeaderContents = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+`;
+
+export const StyledLogo = styled.h1``;
+
+export const StyledBtnSet = styled.div``;
