14fea11a2d220604967ad1567fe6b87aed0caf0b
ahnytae <ahnheetae@ucomp.co.kr>
2021-07-13 22:34:06 +0900

    :tada: setting 레이아웃
     레이아웃,기획서 검토 필요

--- client/src/routes/AppRoute.tsx
+++ client/src/routes/AppRoute.tsx
@@ -4,6 +4,7 @@ import { AppRoutePath } from 'types/routes';
 import { Main } from 'pages/Main';
 import { Detail } from 'pages/Detail';
 import { Login } from 'pages/Login';
+import SettingPage from 'pages/Setting/Setting';
 import MoviePage from 'pages/Movie/Movie';
 import AuthRedirect from 'utils/AuthRedirect';
 import Layout from 'assets/styles/Layout';
@@ -17,6 +18,7 @@ const AppRoute: FC = () => {
           <Route exact path={AppRoutePath.MAIN_PATH} component={Main} />
           <Route path={AppRoutePath.DETAIL_PATH} component={Detail} />
           <Route path={AppRoutePath.MOVIE_PATH} component={MoviePage} />
+          <Route path={AppRoutePath.SETTING_PATH} component={SettingPage} />
         </AuthRedirect>
       </Switch>
     </Layout>
