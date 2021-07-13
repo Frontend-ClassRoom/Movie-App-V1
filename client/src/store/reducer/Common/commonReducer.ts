d554c22a26c91743c5501751cf2b86faadbd4cb6
전병길 <jeonbg@kakao.com>
2021-07-13 11:38:39 +0900

    fix (#12) : commonReducer 수정

--- client/src/store/reducer/Common/commonReducer.ts
+++ client/src/store/reducer/Common/commonReducer.ts
@@ -1,29 +1,18 @@
-import {
-  CloseFixedHeaderACtion,
-  SetFixedHeaderAction,
-  SidebarActionToggle,
-  SidebarCloseAction,
-} from 'store/action';
+import { SidebarActionToggle, SidebarCloseAction } from 'store/action';
 
 export const SET_TOGGLE_SIDEBAR = 'common/SET_TOGGLE_SIDEBAR' as const;
 export const SET_CLOSE_SIDEBAR = 'common/SET_CLOSE_SIDEBAR' as const;
-export const SET_FIXED_HEADER = 'common/SET_FIXED_HEADER' as const;
-export const SET_CLOSE_FIXED_HEADER = 'common/SET_CLOSE_FIXED_HEADER' as const;
 
 export interface CommonState {
   sidebar: boolean;
-  fixedHeader: boolean;
 }
 
 export type CommonActionType =
   | ReturnType<typeof SidebarActionToggle>
-  | ReturnType<typeof SidebarCloseAction>
-  | ReturnType<typeof SetFixedHeaderAction>
-  | ReturnType<typeof CloseFixedHeaderACtion>;
+  | ReturnType<typeof SidebarCloseAction>;
 
 const initialState: CommonState = {
   sidebar: false,
-  fixedHeader: false,
 };
 
 const commonReducer = (
@@ -40,16 +29,6 @@ const commonReducer = (
       return {
         ...state,
         sidebar: false,
-      };
-    case 'common/SET_FIXED_HEADER':
-      return {
-        ...state,
-        fixedHeader: true,
-      };
-    case 'common/SET_CLOSE_FIXED_HEADER':
-      return {
-        ...state,
-        fixedHeader: false,
       };
     default:
       return state;
