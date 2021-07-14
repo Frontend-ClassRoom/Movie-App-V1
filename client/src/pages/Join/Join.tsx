60e4f487a121f7d8e6cb127a28adb697a816aff6
전병길 <jeonbg@kakao.com>
2021-07-14 21:46:43 +0900

    feat : 회원가입 페이지 임시추가
    1. 회원가입 페이지 추가
    2. 회원가입 로직 추가 ( 임시 )

---
+++ client/src/pages/Join/Join.tsx
@@ -0,0 +1,69 @@
+import { ChangeEvent, useCallback, useState } from 'react';
+import { createUserAccount } from 'api/userApi';
+import { InputBox } from 'component';
+import { Account } from 'types/user';
+import useAsync from 'hook/useAsync';
+
+const Join = () => {
+  const [account, setAccount] = useState<Account>({
+    id: '',
+    name: '',
+    password: '',
+  });
+  const { execute, data } = useAsync(() => createUserAccount(account), false);
+
+  const onChangeAccount = useCallback(
+    (e: ChangeEvent<HTMLInputElement>) => {
+      const { value, name } = e.target;
+      setAccount({
+        ...account,
+        [name]: value,
+      });
+    },
+    [account]
+  );
+
+  function validation<T>(obj: T) {
+    const checkValidation = Object.values(obj)
+      .map((value) => {
+        return value.trim().length;
+      })
+      .find((value: number) => value === 0);
+    return checkValidation === 0 ? false : true;
+  }
+
+  const onSubmit = useCallback(() => {
+    if (validation<Account>(account)) {
+      execute();
+    }
+  }, [account]);
+
+  return (
+    <div>
+      <button onClick={onSubmit}>123</button>
+      <InputBox
+        value={account.id}
+        name="id"
+        type="text"
+        onChange={onChangeAccount}
+        submit={(e) => console.log(e)}
+      />
+      <InputBox
+        value={account.name}
+        name="name"
+        type="text"
+        onChange={onChangeAccount}
+        submit={(e) => console.log(e)}
+      />
+      <InputBox
+        value={account.password}
+        name="password"
+        type="password"
+        onChange={onChangeAccount}
+        submit={(e) => console.log(e)}
+      />
+    </div>
+  );
+};
+
+export default Join;
