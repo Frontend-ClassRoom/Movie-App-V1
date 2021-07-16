aafa3a09ab9d18ad2767ab19a07db7b6a6092a4a
전병길 <jeonbg@kakao.com>
2021-07-16 23:18:28 +0900

    feat : 회원가입 페이지 레이아웃 추가 1. 스타일 추가 2. 비밀번호 확인 인풋박스 추가

--- client/src/pages/Join/Join.tsx
+++ client/src/pages/Join/Join.tsx
@@ -1,11 +1,14 @@
 import { ChangeEvent, useCallback, useEffect, useState } from 'react';
 import { createUserAccount } from 'api/userApi';
-import { InputBox } from 'component';
+import { Button, InputBox } from 'component';
 import { Account } from 'types/user';
 import useAsync from 'hook/useAsync';
 import { useHistory } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { RootState } from 'store/reducer';
+import { StyledWrapper } from 'pages/Login/Styled';
+import { JoinForm, StyledTitle } from './Styled';
+import { validation } from 'utils/ValidationObject';
 
 const Join = () => {
   const history = useHistory();
@@ -14,6 +17,7 @@ const Join = () => {
     id: '',
     name: '',
     password: '',
+    confirmPassword: '',
   });
 
   useEffect(() => {
@@ -38,46 +42,76 @@ const Join = () => {
     [account]
   );
 
-  function validation<T>(obj: T) {
-    const checkValidation = Object.values(obj)
-      .map((value) => {
-        return value.trim().length;
-      })
-      .find((value: number) => value === 0);
-    return checkValidation === 0 ? false : true;
-  }
+  const onSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+      const { password, confirmPassword } = account;
 
-  const onSubmit = useCallback(() => {
-    if (validation<Account>(account)) {
-      submit();
-    }
-  }, [account]);
+      if (password !== confirmPassword) {
+        alert('비밀번호가 같아야합니다.');
+        return;
+      }
+
+      if (validation<Account>(account)) {
+        alert('아직 백엔드 로그인 로직이 머지전이라 에러가 나는게 정상입니다.');
+        submit();
+        /**
+         * submit 후에 return data를 확인해서 history.push('/login') 하거나 로직 협의
+         */
+      }
+    },
+    [account, data]
+  );
 
   return (
-    <div>
-      <button onClick={onSubmit}>123</button>
-      <InputBox
-        value={account.id}
-        name="id"
-        type="text"
-        onChange={onChangeAccount}
-        submit={onSubmit}
-      />
-      <InputBox
-        value={account.name}
-        name="name"
-        type="text"
-        onChange={onChangeAccount}
-        submit={onSubmit}
-      />
-      <InputBox
-        value={account.password}
-        name="password"
-        type="password"
-        onChange={onChangeAccount}
-        submit={onSubmit}
-      />
-    </div>
+    <StyledWrapper>
+      <StyledTitle>SIGN UP</StyledTitle>
+      <JoinForm onSubmit={onSubmit}>
+        <InputBox
+          className="join-form"
+          value={account.id}
+          name="id"
+          type="text"
+          placeholder="아이디를 입력해주세요."
+          onChange={onChangeAccount}
+          submit={onSubmit}
+        />
+        <InputBox
+          className="join-form"
+          value={account.name}
+          name="name"
+          type="text"
+          placeholder="사용하실 닉네임을 입력해주세요."
+          onChange={onChangeAccount}
+          submit={onSubmit}
+        />
+        <InputBox
+          className="join-form"
+          value={account.password}
+          name="password"
+          type="password"
+          placeholder="비밀번호를 입력해주세요."
+          onChange={onChangeAccount}
+          submit={onSubmit}
+        />
+        <InputBox
+          className="join-form"
+          value={account.confirmPassword}
+          name="confirmPassword"
+          type="password"
+          placeholder="비밀번호를 한번더 입력해주세요."
+          onChange={onChangeAccount}
+          submit={onSubmit}
+        />
+        <Button
+          variant="primary"
+          size="large"
+          style={{ width: '100%' }}
+          onClick={onSubmit}
+          label="회원가입"
+        />
+      </JoinForm>
+    </StyledWrapper>
   );
 };
 
--- client/src/types/user.ts
+++ client/src/types/user.ts
@@ -2,4 +2,5 @@ export interface Account {
   id: string;
   name: string;
   password: string;
+  confirmPassword: string;
 }
