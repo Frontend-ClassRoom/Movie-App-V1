1e1b95d1908771a099b4008cbfbaaa598809bc10
전병길 <jeonbg@kakao.com>
2021-08-19 23:56:28 +0900

    feature : Card 페이지 추가

--- client/src/pages/Card/CardPage.tsx
+++ client/src/pages/Card/CardPage.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 const CardPage = () => {
+  const history = useHistory();
   return (
     <>
+      <button onClick={() => history.push('/write')}>Post 생성</button>
       <p>Card</p>
     </>
   );
