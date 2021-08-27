c9d2b5118787d12c471c5e160a6511dd8736c5ee
전병길 <jeonbg@kakao.com>
2021-08-27 23:42:11 +0900

    feature : CardList 전체삭제 기능 임시추가

--- client/src/pages/Card/CardPage.tsx
+++ client/src/pages/Card/CardPage.tsx
@@ -1,19 +1,29 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from 'store/reducer';
 import { AppRoutePath } from 'types/routes';
 import CardList from 'component/Card/CardList';
 import { StyledCardContainer } from './Styled';
+import { ClearPostAction } from 'store/action/Posts';
 
 const CardPage = () => {
   const { posts } = useSelector((state: RootState) => state.PostReducer);
   const history = useHistory();
+  const dispatch = useDispatch();
+
+  const handleClearPost = () => {
+    if (posts.length === 0) return;
+    dispatch(ClearPostAction());
+  };
 
   return (
     <>
       <button onClick={() => history.push(AppRoutePath.WRITE_PATH)}>
         Post 생성
+      </button>
+      <button onClick={handleClearPost} title="전체 삭제">
+        전체 삭제
       </button>
       <StyledCardContainer>
         {posts &&
